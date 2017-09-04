17658f3c5e92302b5b567d91d52735635901ac5d
root <root@OisaOpenERP.(none)>
2017-09-04 16:19:07 -0500

    cambio temporal en la condición de las fechas para la primera vez que se mete

--- inciden.py
+++ inciden.py
@@ -13,27 +13,31 @@ cr.execute("""insert into asistmil_inciden(empleado,fecha,tipo,secuencia)
               select empleado,fecha,'0355',0
               from asistmil_incidencias
               where substring(horario from 1 for 1) in ('1','2','3','4','5','6','7','8','9')
-              and fecha >= date_trunc('month', now())""");
+              and extract(year from fecha) = extract(year from now())
+              and extract(month from fecha) <= extract(month from now())""");
 
 #Los registros de la tabla Autorizaciones entran con Tipo igual a la clave del justificante
 cr.execute("""insert into asistmil_inciden(empleado,fecha,tipo,tiempo,secuencia)
               select empleado,fecha,lpad(justificante::text, 4, '0'),0,0
               from asistmil_autorizaciones
-              where fecha >= date_trunc('month', now())""")
+              where extract(year from fecha) = extract(year from now())
+              and extract(month from fecha) <= extract(month from now())""");
 
 #Los retardos tienen tipo 0599 y se guarda el tiempo
 cr.execute("""insert into asistmil_inciden(empleado,fecha,tipo,tiempo,secuencia)
               select empleado,fecha,'0599',retardo,0
               from asistmil_incidencias
               where retardo is not null 
-              and fecha >= date_trunc('month', now())""")
+              and extract(year from fecha) = extract(year from now())
+              and extract(month from fecha) <= extract(month from now())""");
 
 #Los anticipados tienen tipo 0600 y se guarda el tiempo
 cr.execute("""insert into asistmil_inciden(empleado,fecha,tipo,tiempo,secuencia)
               select empleado,fecha,'0600',anticipado,0
               from asistmil_incidencias
               where anticipado is not null 
-              and fecha >= date_trunc('month', now())""")
+              and extract(year from fecha) = extract(year from now())
+              and extract(month from fecha) <= extract(month from now())""");
 
 #Las horas extras entran con tipo 0699, son los que tienen el valor TIEMPO EXTRA en el campo horario y se guarda el tiempo
 cr.execute("""insert into asistmil_inciden(empleado,fecha,tipo,tiempo,secuencia)
@@ -41,14 +45,16 @@ cr.execute("""insert into asistmil_inciden(empleado,fecha,tipo,tiempo,secuencia)
               from asistmil_incidencias
               where horario = 'TIEMPO EXTRA'
               and extras is not null
-              and fecha >= date_trunc('month', now())""")
+              and extract(year from fecha) = extract(year from now())
+              and extract(month from fecha) <= extract(month from now())""");
 
 #Las faltas entran con tipo 0000 y son las que el horario sea FALTA
 cr.execute("""insert into asistmil_inciden(empleado,fecha,tipo,tiempo,secuencia)
               select empleado,fecha,'0000',0,0
               from asistmil_incidencias
               where horario = 'FALTA'
-              and fecha >= date_trunc('month', now())""")
+              and extract(year from fecha) = extract(year from now())
+              and extract(month from fecha) <= extract(month from now())""");
 
 #Borrar los que tengan falta (0000) y la fecha sea menor a la fecha de alta del empleado o sea un día marcado como vacaciones
 cr.execute("""delete from asistmil_inciden t1
